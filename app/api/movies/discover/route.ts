bd2e8299bb1b765a92834c9c77cfc8e75228a632
FEI3 <Fatimazahra.ELYOUNOUSSI@3ds.com>
2024-11-06 23:43:23 +0100

    Initial commit

--- app/api/movies/discover/route.ts
+++ app/api/movies/discover/route.ts
@@ -1,4 +1,4 @@
-import { NextApiRequest, NextApiResponse } from 'next';
+import { NextResponse } from 'next/server';
 import axios from 'axios';
 
 interface Movie {
@@ -17,17 +17,14 @@ interface DiscoverMoviesResponse {
   total_results: number;
 }
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  if (req.method !== 'GET') {
-    return res.status(405).json({ error: 'Method Not Allowed' });
-  }
-
+// La fonction GET pour gérer les requêtes GET sur cette route
+export async function GET(req: Request) {
   try {
     const apiKey = process.env.TMDB_API_KEY || 'YOUR_API_KEY';
-    const { page = 1, sort_by = 'popularity.desc', language = 'fr-FR' } = req.query;
+    const { searchParams } = new URL(req.url);
+    const page = searchParams.get('page') || '1';
+    const sort_by = searchParams.get('sort_by') || 'popularity.desc';
+    const language = searchParams.get('language') || 'fr-FR';
 
     const response = await axios.get<DiscoverMoviesResponse>('https://api.themoviedb.org/3/discover/movie', {
       params: {
@@ -38,9 +35,9 @@ export default async function handler(
       },
     });
 
-    res.status(200).json(response.data);
+    return NextResponse.json(response.data);
   } catch (error) {
     console.error('Erreur lors de la récupération des films :', error);
-    res.status(500).json({ error: 'Erreur lors de la récupération des films' });
+    return NextResponse.json({ error: 'Erreur lors de la récupération des films' }, { status: 500 });
   }
 }
