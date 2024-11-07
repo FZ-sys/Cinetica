49401af7985c5770f1a223e0e3d3a17a86310ed0
FEI3 <Fatimazahra.ELYOUNOUSSI@3ds.com>
2024-11-07 11:53:17 +0100

    feature: Discover added

--- app/api/shows/discover/route.ts
+++ app/api/shows/discover/route.ts
@@ -1,43 +1,15 @@
 import { NextResponse } from 'next/server';
 import axios from 'axios';
 
-interface Movie {
-  id: number;
-  title: string;
-  overview: string;
-  release_date: string;
-  popularity: number;
-  poster_path: string;
-}
+export async function GET() {
+  const API_KEY = process.env.NEXT_PUBLIC_TMDB_API_KEY;
+  const url = `https://api.themoviedb.org/3/discover/tv?api_key=${API_KEY}&language=fr-FR&page=1`;
 
-interface DiscoverMoviesResponse {
-  page: number;
-  results: Movie[];
-  total_pages: number;
-  total_results: number;
-}
-
-// La fonction GET pour gérer les requêtes GET sur cette route
-export async function GET(req: Request) {
   try {
-    const apiKey = process.env.TMDB_API_KEY || 'YOUR_API_KEY';
-    const { searchParams } = new URL(req.url);
-    const page = searchParams.get('page') || '1';
-    const sort_by = searchParams.get('sort_by') || 'popularity.desc';
-    const language = searchParams.get('language') || 'fr-FR';
-
-    const response = await axios.get<DiscoverMoviesResponse>('https://api.themoviedb.org/3/discover/tv', {
-      params: {
-        api_key: apiKey,
-        language,
-        sort_by,
-        page,
-      },
-    });
-
+    const response = await axios.get(url);
     return NextResponse.json(response.data);
   } catch (error) {
-    console.error('Erreur lors de la récupération des films :', error);
-    return NextResponse.json({ error: 'Erreur lors de la récupération des films' }, { status: 500 });
+    console.error('Error fetching now-playing movies:', error);
+    return NextResponse.json({ error: 'Failed to fetch data' }, { status: 500 });
   }
 }
